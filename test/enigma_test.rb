ce5978e5b80308eb1c86f4b8b7a1218d0c5554bb
Will Dunlap <dunlapww@gmail.com>
2020-09-19 12:27:51 -0600

    Add test it has attributes

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,6 +7,13 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
+  def test_it_has_attributes
+    enigma = Enigma.new({message: "hello end",key: "02938",date: "040895"})
+    assert_equal "hello end", enigma.message
+    assert_equal "02938", enigma.key
+    assert_equal "040895", enigma.date
+  end
+
 
 
 end

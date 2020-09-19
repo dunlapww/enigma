1bcea85896707b3512f0fbe12b90361a07c152d5
Will Dunlap <dunlapww@gmail.com>
2020-09-19 12:24:39 -0600

    Pass test it exists

---
+++ lib/enigma.rb
@@ -0,0 +1,9 @@
+class Enigma
+
+  def initialize(details)
+    @message = details[:message]
+    @key = details[:key]
+    @date = details[:date]
+  end
+
+end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,7 +3,7 @@ require './test/test_helper'
 class EnigmaTest < Minitest::Test
 
   def test_it_exists
-    enigma = Enigma.new("hello end", "02938", "040895")
+    enigma = Enigma.new({message: "hello end",key: "02938",date: "040895"})
     assert_instance_of Enigma, enigma
   end
 

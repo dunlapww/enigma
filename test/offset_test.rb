199700568bf2cf7bfacf0113f225063681224f58
Will Dunlap <dunlapww@gmail.com>
2020-09-20 17:21:07 -0600

    Add test can return the squared date as array of nums

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -16,4 +16,10 @@ class OffsetTest < Minitest::Test
     offset = Offset.new("040895")
     assert_equal "1672401025", offset.square_date
   end
+
+  def test_it_can_convert_squared_date_to_array_of_ints
+    offset = Offset.new("040895")
+    offset.stubs(:square_date).returns("102035423")
+    assert_equal [1,0,2,0,3,5,4,2,3], offset.squared_date_nums
+  end
 end

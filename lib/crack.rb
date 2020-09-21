4cbdbf899b06b7f31737f1c67b351de3ff793916
Will Dunlap <dunlapww@gmail.com>
2020-09-21 12:59:07 -0600

    Pass it has attributes

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,18 +1,19 @@
 class Crack
-  attr_reader :offsets
+  attr_reader :message, :date
 
   def initialize(message, date)
     @message = message
     @date = date
-    @offsets = set_offsets(date)
   end
 
-  def set_offsets(date)
-    offset = Offset.new(date)
+  def offsets
+    offset = Offset.new(@date)
     offset.squared_date_nums.pop(4)
   end
 
-  
+  def end_offsets
+  end
+
 
   def decoded_message
   end
--- test/crack_test.rb
+++ test/crack_test.rb
@@ -7,14 +7,21 @@ class CrackTest < Minitest::Test
     assert_instance_of Crack, crack
   end
 
+  def test_it_has_attributes
+    crack = Crack.new("vjqtbeaweqihssi","291018")
+    assert_equal "vjqtbeaweqihssi", crack.message
+    assert_equal "291018", crack.date
+  end
+
+
   def test_it_can_decode_message
     crack = Crack.new("vjqtbeaweqihssi","291018")
     assert_equal "hello world end", crack.decoded_message
   end
 
-  def test_it_can_set_offsets
+  def test_it_can_calc_offsets
     crack = Crack.new("vjqtbeaweqihssi","291018")
-    assert_equal [6, 3, 2, 4], crack.set_offsets("291018")
+    assert_equal [6, 3, 2, 4], crack.offsets
   end
 
   def test_it_knows_offset_for__end

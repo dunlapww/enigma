a4c45fbc268b14257ae9755fef2d3daec09a26fe
Will Dunlap <dunlapww@gmail.com>
2020-09-20 20:02:10 -0600

    Add test it can decrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,6 +17,15 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, enigma.encrypt("hello world","02715","040895")
   end
 
+  def test_it_can_decrypt
+    enigma = Enigma.new()
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.decrypt("keder ohulw","02715","040895")
+  end
 
 
   # def test_it_can_encode_a_message

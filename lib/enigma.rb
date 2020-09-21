80493def1fc955e13a2c1ee9ded6369c44373ab3
Will Dunlap <dunlapww@gmail.com>
2020-09-20 19:39:31 -0600

    Add test it can encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,35 +1,12 @@
 class Enigma
-  attr_reader :message, :key, :date, :shift
 
-  def initialize(details)
-    @message = details[:message]
-    @key = details.fetch(:key, rand(99999).to_s.rjust(5,'0'))
-    @date = details.fetch(:date, Date.today.strftime("%d%m%y"))
-    @shift = Shift.new(Key.new(@key), Offset.new(@date))
-    @alphabet = Alphabet.new
-  end
+
+end
 
 
 
-  def final_shifts
-    @shift.shifts.map do |shift|
-      shift % @alphabet.size
-    end
-  end
 
-  def message_to_nums
-    @message.chars.map do |char|
-      @alphabet.alpha_to_num[char]
-    end
-  end
 
-  def encode_shift
-    counter = -1
-    message_to_nums.map do |num|
-      counter += 1
-      (num + final_shifts[counter % final_shifts.size]) % alpha_to_num.size
-    end
-  end
   #
   # def encode_message
   #   encode_shift.reduce("") do |memo, num|
@@ -47,7 +24,3 @@ class Enigma
   #
   # def decode_shift
   # end
-
-
-
-end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,35 +3,36 @@ require './test/test_helper'
 class EnigmaTest < Minitest::Test
 
   def test_it_exists
-    enigma = Enigma.new({message: "hello end",key: "02938",date: "040895"})
+    enigma = Enigma.new()
     assert_instance_of Enigma, enigma
   end
 
-  
-
-  def test_it_can_encode_a_message
-    skip
-    enigma = Enigma.new({message: "hello world",key: "02715",date: "040895"})
-    assert_equal "keder ohulw", enigma.encode_message
-  end
-
   def test_it_can_encrypt
-    skip
-    enigma = Enigma.new({message: "hello world",key: "02715",date: "040895"})
+    enigma = Enigma.new()
     expected = {
       encryption: "keder ohulw",
       key: "02715",
       date: "040895"
     }
-    assert_equal expected, enigma.encrypt
+    assert_equal expected, enigma.encrypt("hello world","02715","040895")
   end
 
-  def test_it_can_apply_decode_shift
-    skip
-    enigma = Enigma.new({message: "hello world",key: "02715",date: "040895"})
-    expected = [7,4,11,11,14,26,22,14,17,11,3]
-    assert_equal expected, enigma.decode_shift
-  end
+
+
+  # def test_it_can_encode_a_message
+  #   skip
+  #   enigma = Enigma.new({message: "hello world",key: "02715",date: "040895"})
+  #   assert_equal "keder ohulw", enigma.encode_message
+  # end
+  #
+  #
+  #
+  # def test_it_can_apply_decode_shift
+  #   skip
+  #   enigma = Enigma.new({message: "hello world",key: "02715",date: "040895"})
+  #   expected = [7,4,11,11,14,26,22,14,17,11,3]
+  #   assert_equal expected, enigma.decode_shift
+  # end
 
 
 
